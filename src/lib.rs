a9c1730afab6aef0478b73ab0be726417291ab8a
Rahul <basicfunc@gmail.com>
2023-06-29 01:37:52 +0530

    Added ScoopInfo as helper

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,8 @@ pub mod uninstall;
 pub mod update;
 pub mod which;
 
+use std::{env, fmt, path::PathBuf};
+
 // External Crate Imports.
 use argh::FromArgs;
 
@@ -68,4 +70,95 @@ pub enum Command {
     Uninstall(UninstallCommand),
     Update(UpdateCommand),
     Which(WhichCommand),
+}
+
+#[derive(Debug)]
+pub enum ScoopieDirError {
+    HomeDirError,
+    ConfigDirError,
+    EnvVarError(String),
+    InvalidFormatError,
+}
+
+impl std::error::Error for ScoopieDirError {}
+
+impl fmt::Display for ScoopieDirError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            ScoopieDirError::HomeDirError => write!(f, "Unable to get $HOME dir."),
+            ScoopieDirError::ConfigDirError => write!(f, "Unable to get $CONFIG dir."),
+            ScoopieDirError::EnvVarError(var_name) => {
+                write!(f, "Error retrieving environment variable: {}", var_name)
+            }
+            ScoopieDirError::InvalidFormatError => {
+                write!(f, "Invalid format for environment variable.")
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ScoopieInfo {
+    pub home: PathBuf,
+    pub apps: PathBuf,
+    pub buckets: PathBuf,
+    pub cache: PathBuf,
+    pub persist: PathBuf,
+    pub shims: PathBuf,
+    pub config: PathBuf,
+}
+
+impl fmt::Display for ScoopieInfo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "Scoopie at: {}\nConfigs at: {}",
+            self.home.display(),
+            self.config.display()
+        )
+    }
+}
+
+impl ScoopieInfo {
+    pub fn get() -> Result<Self, ScoopieDirError> {
+        let home = Self::prefix()?;
+        let config = Self::config()?;
+        let apps = home.join("apps");
+        let buckets = home.join("buckets");
+        let cache = home.join("cache");
+        let persist = home.join("persist");
+        let shims = home.join("shims");
+
+        Ok(ScoopieInfo {
+            home,
+            apps,
+            buckets,
+            cache,
+            persist,
+            shims,
+            config,
+        })
+    }
+
+    pub fn prefix() -> Result<PathBuf, ScoopieDirError> {
+        let home_dir = dirs::home_dir().ok_or(ScoopieDirError::HomeDirError)?;
+
+        let scoopie_default_path = home_dir.join(DEFAULT_PREFIX);
+
+        let path = match env::var(PREFIX_KEY) {
+            Ok(path) => PathBuf::from(path),
+            Err(e) => match e {
+                env::VarError::NotPresent => scoopie_default_path,
+                env::VarError::NotUnicode(_) => return Err(ScoopieDirError::InvalidFormatError),
+            },
+        };
+
+        Ok(path)
+    }
+
+    pub fn config() -> Result<PathBuf, ScoopieDirError> {
+        let config_dir = dirs::config_dir().ok_or(ScoopieDirError::ConfigDirError)?;
+        let config_dir = config_dir.join(DEFAULT_PREFIX);
+        Ok(config_dir)
+    }
 }
