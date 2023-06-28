e5065622593b5eb89fcb4ed240b3cc480984f4a5
Rahul <basicfunc@gmail.com>
2023-06-29 01:35:38 +0530

    Enhanced init subcommand.

--- src/init.rs
+++ src/init.rs
@@ -19,6 +19,9 @@ pub enum InitErrors {
     ConfigDirUnavailable,
     UnableToCreateDir(PathBuf),
     UnableToSetEnvVar,
+    NotFound(PathBuf),
+    PermissionDenied(PathBuf),
+    Unknown,
 }
 
 impl std::error::Error for InitErrors {}
@@ -33,6 +36,11 @@ impl std::fmt::Display for InitErrors {
             Self::UnableToSetEnvVar => {
                 write!(f, "Unable to set environment variable $SCOOPIE_HOME")
             }
+            Self::NotFound(file) => write!(f, "Unable to find file: {}", file.display()),
+            Self::PermissionDenied(file) => {
+                write!(f, "Permission Denied to access file: {}", file.display())
+            }
+            _ => write!(f, "Unknown!"),
         }
     }
 }
@@ -57,35 +65,71 @@ impl std::fmt::Display for InitSuccess {
 
 impl InitCommand {
     pub fn from(config: &InitCommand) -> Result<InitSuccess, InitErrors> {
-        let path = config.path.clone();
+        let scoopie_path = config.path.clone();
 
         let home_dir = home_dir().ok_or(InitErrors::HomeDirUnavailable)?;
 
         let config_dir = config_dir().ok_or(InitErrors::ConfigDirUnavailable)?;
         let config_dir = config_dir.join(DEFAULT_PREFIX);
 
-        let path = path
+        let path = scoopie_path
             .map(PathBuf::from)
             .unwrap_or_else(|| home_dir.join(DEFAULT_PREFIX));
+
+        let apps_dir = path.join("apps");
+        let buckets_dir = path.join("buckets");
+        let buckets_json = buckets_dir.join("buckets.json");
+        let cache_dir = path.join("cache");
+        let persist_dir = path.join("persist");
+        let shims_dir = path.join("shims");
 
         if path.exists() {
             return Err(InitErrors::DirAlreadyExists(path.clone()));
         }
 
-        DirBuilder::new()
-            .recursive(true)
+        let mut builder = DirBuilder::new();
+        let builder = builder.recursive(true);
+
+        builder
             .create(&path)
             .map_err(|_| InitErrors::UnableToCreateDir(path.clone()))?;
 
-        DirBuilder::new()
-            .recursive(true)
-            .create(&config_dir)
-            .map_err(|_| InitErrors::UnableToCreateDir(config_dir.clone()))?;
+        builder
+            .create(&apps_dir)
+            .map_err(|_| InitErrors::UnableToCreateDir(apps_dir.clone()))?;
 
-        let value = path.display().to_string();
+        builder
+            .create(&buckets_dir)
+            .map_err(|_| InitErrors::UnableToCreateDir(buckets_dir.clone()))?;
+
+        builder
+            .create(&cache_dir)
+            .map_err(|_| InitErrors::UnableToCreateDir(cache_dir.clone()))?;
+
+        builder
+            .create(&persist_dir)
+            .map_err(|_| InitErrors::UnableToCreateDir(persist_dir.clone()))?;
+
+        builder
+            .create(&shims_dir)
+            .map_err(|_| InitErrors::UnableToCreateDir(shims_dir.clone()))?;
+
+        std::fs::write(&buckets_json, "").map_err(|err| match err.kind() {
+            std::io::ErrorKind::NotFound => InitErrors::NotFound(buckets_json),
+            std::io::ErrorKind::PermissionDenied => InitErrors::PermissionDenied(buckets_json),
+            _ => InitErrors::Unknown,
+        })?;
+
+        if !config_dir.exists() {
+            builder
+                .create(&config_dir)
+                .map_err(|_| InitErrors::UnableToCreateDir(config_dir.clone()))?;
+        }
+
+        let scoopie_path = path.display().to_string();
 
         let output = Command::new("cmd")
-            .args(&["/C", "setx", PREFIX_KEY, &value])
+            .args(&["/C", "setx", PREFIX_KEY, &scoopie_path])
             .output()
             .map_err(|_| InitErrors::UnableToSetEnvVar)?;
 
