f66072082bb93c1075afd5d728240bbdc8ad97b0
Rahul <basicfunc@gmail.com>
2023-07-08 21:43:38 +0530

    Made implementation more robust

--- src/query.rs
+++ src/query.rs
@@ -1,12 +1,13 @@
+use std::path::PathBuf;
+
 use argh::FromArgs;
 use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
-use regex::RegexBuilder;
-use rusqlite::{params, Connection};
+use rusqlite::{params, Connection, Error, Row};
 use serde::{Deserialize, Serialize};
 use serde_json::{self, Value};
 
 #[derive(FromArgs, PartialEq, Debug)]
-/// Search available apps (supports regex and full-text search)
+/// Search available apps (supports full-text search)
 #[argh(subcommand, name = "query")]
 pub struct QueryCommand {
     #[argh(positional)]
@@ -19,14 +20,7 @@ use crate::{
 };
 
 #[derive(Debug)]
-enum QueryType {
-    AppName,
-    Regex,
-    FullText,
-}
-
-#[derive(Debug)]
-struct Entry {
+struct QueryResult {
     app_name: String,
     mainfest: String,
 }
@@ -40,18 +34,34 @@ pub struct AppInfo {
     binaries: String,
 }
 
-impl QueryType {
-    fn from(query: &str) -> QueryType {
-        if RegexBuilder::new(query)
-            .build()
-            .is_ok_and(|x| x.is_match(""))
-        {
-            QueryType::Regex
-        } else if query.contains(" ") {
-            QueryType::FullText
-        } else {
-            QueryType::AppName
-        }
+impl AppInfo {
+    fn from(repo: &PathBuf, value: QueryResult) -> Result<AppInfo, ScoopieError> {
+        let app_name = value.app_name;
+        let mainfest: Value = serde_json::from_str(&value.mainfest)
+            .map_err(|_| ScoopieError::Query(QueryError::InavlidJSONData))?;
+
+        let db_name = &repo
+            .file_name()
+            .unwrap_or_default()
+            .to_string_lossy()
+            .to_string();
+        let repo_name = db_name.split('-').next().unwrap_or_default().to_string();
+
+        let description = mainfest
+            .get("description")
+            .unwrap_or(&Value::Null)
+            .to_string();
+
+        let binaries = mainfest.get("bin").unwrap_or(&Value::Null).to_string();
+        let version = mainfest.get("version").unwrap_or(&Value::Null).to_string();
+
+        Ok(AppInfo {
+            repo_name,
+            app_name,
+            version,
+            description,
+            binaries,
+        })
     }
 }
 
@@ -60,10 +70,9 @@ impl QueryCommand {
         let q = query.query;
         let q = q.trim();
 
-        let result = match QueryType::from(q) {
-            QueryType::AppName => Self::query_app(q),
-            QueryType::Regex => Self::regex(q),
-            QueryType::FullText => Self::full_text_search(q),
+        let result = match q.contains(" ") {
+            true => Self::full_text_search(q),
+            false => Self::query_app(q),
         };
 
         let result = result?;
@@ -71,80 +80,61 @@ impl QueryCommand {
         Ok(result.concat())
     }
 
-    fn query_app(app_name: &str) -> Result<Vec<Vec<AppInfo>>, ScoopieError> {
-        Self::query(&format!("%{app_name}%"))
-    }
-
     fn full_text_search(query: &str) -> Result<Vec<Vec<AppInfo>>, ScoopieError> {
-        println!("Query: {}", query);
-        Ok(vec![])
+        Query::builder("SELECT app_name, mainfest FROM mainfests_fts WHERE mainfest MATCH (?)")?
+            .run(query)
     }
 
-    fn regex(re: &str) -> Result<Vec<Vec<AppInfo>>, ScoopieError> {
-        println!("Regex: {}", re);
-        Ok(vec![])
+    fn query_app(app_name: &str) -> Result<Vec<Vec<AppInfo>>, ScoopieError> {
+        Query::builder("SELECT app_name, mainfest FROM mainfests WHERE app_name LIKE ?")?
+            .run(&format!("%{app_name}%"))
     }
+}
 
-    fn query(app_name: &str) -> Result<Vec<Vec<AppInfo>>, ScoopieError> {
+struct Query {
+    repos: Vec<PathBuf>,
+    stmt: String,
+}
+
+impl Query {
+    fn builder(stmt: &str) -> Result<Query, ScoopieError> {
         let repos = Config::read()?.latest_repos()?;
+        Ok(Self {
+            repos,
+            stmt: stmt.into(),
+        })
+    }
 
-        let all_results: Result<Vec<Vec<AppInfo>>, ScoopieError> = repos
+    fn run(&self, params: &str) -> Result<Vec<Vec<AppInfo>>, ScoopieError> {
+        let fetch_row = |row: &Row| -> Result<QueryResult, Error> {
+            let app_name = row.get(0)?;
+            let mainfest = row.get(1)?;
+            Ok(QueryResult { app_name, mainfest })
+        };
+
+        self.repos
             .par_iter()
             .map(|repo| -> Result<Vec<AppInfo>, ScoopieError> {
                 let conn = Connection::open(&repo)
                     .map_err(|_| ScoopieError::Database(DatabaseError::UnableToOpen))?;
 
                 let mut stmt = conn
-                    .prepare("SELECT app_name, mainfest FROM mainfests WHERE app_name LIKE ?")
+                    .prepare(&self.stmt)
                     .map_err(|_| ScoopieError::Database(DatabaseError::FailedToMkStmt))?;
 
                 let rows = stmt
-                    .query_map(params![app_name], |row| {
-                        let app_name = row.get(0)?;
-                        let mainfest = row.get(1)?;
-                        Ok(Entry { app_name, mainfest })
-                    })
+                    .query_map(params![params], fetch_row)
                     .map_err(|_| ScoopieError::Query(QueryError::FailedToQuery))?;
 
-                let results: Result<Vec<AppInfo>, ScoopieError> = rows
-                    .into_iter()
+                rows.into_iter()
                     .map(|row| -> Result<AppInfo, ScoopieError> {
                         let row =
                             row.map_err(|_| ScoopieError::Query(QueryError::FailedToRetrieveData))?;
 
-                        let app_name = row.app_name;
-                        let mainfest: Value = serde_json::from_str(&row.mainfest)
-                            .map_err(|_| ScoopieError::Query(QueryError::InavlidJSONData))?;
-
-                        let db_name = &repo
-                            .file_name()
-                            .unwrap_or_default()
-                            .to_string_lossy()
-                            .to_string();
-                        let repo_name = db_name.split('-').nth(0).unwrap_or_default().to_string();
-
-                        let description = mainfest
-                            .get("description")
-                            .unwrap_or(&Value::Null)
-                            .to_string();
-
-                        let binaries = mainfest.get("bin").unwrap_or(&Value::Null).to_string();
-                        let version = mainfest.get("version").unwrap_or(&Value::Null).to_string();
-
-                        Ok(AppInfo {
-                            repo_name,
-                            app_name,
-                            version,
-                            description,
-                            binaries,
-                        })
+                        AppInfo::from(&repo, row)
                     })
-                    .collect();
-
-                results
+                    .collect()
             })
-            .collect();
-
-        all_results
+            .collect()
     }
 }
